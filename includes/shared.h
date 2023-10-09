b55ae3556dcca63f0d3d003d6f2a4418501a01d9
Arteandr <artedamatik@gmail.com>
2023-10-09 12:50:22 +0300

    Add alliasses

--- includes/shared.h
+++ includes/shared.h
@@ -1,6 +1,10 @@
 #ifndef SHARED_H
 #define SHARED_H
 
-typedef unsigned char byte;
+#include <cstdint>
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
 
+using byte = u8;
 #endif // !SHARED_H
